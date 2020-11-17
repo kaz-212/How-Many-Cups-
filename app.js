6232e14dd0e28257ac747de642108781fb98120a
Karan Basu <karan.basu16@gmail.com>
2020-11-17 15:13:24 +0000

    configured ejs

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const express = require('express')
 const path = require('path')
+const ejsMate = require('ejs-mate')
+
 const app = express()
 // express config
-app.set('view engine', 'html')
+app.engine('ejs', ejsMate)
+app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
 app.use(express.static(path.join(__dirname, 'public')))
 // ======== ROUTES ========
 app.get('/cups', async (req, res) => {
-  res.sendFile('views/cups.html', { root: __dirname })
+  res.render('cups', { root: __dirname })
 })
 
 app.listen(3000, () => {
